ee52024460bb3e6b575c0c92f9618548ee9452c5
KennyDew <dubrovskiynv@gmail.com>
2024-08-06 00:01:17 +0300

    Добавьте файлы проекта.

---
+++ HW_Parallel/Calculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HW_Parallel
+{
+    public class Calculator
+    {
+        
+        /// <summary>
+        /// Обычное вычисление суммы элементов
+        /// </summary>
+        /// <param name="longArray"></param>
+        /// <returns></returns>
+        public long GetSumAsSequential(long[] longArray)
+        {
+            long Sum = 0;
+            foreach (long intValue in longArray)
+            {
+                Sum += intValue;
+            }
+            return Sum;
+        }
+
+        private async Task<long> GetSumInPartArray(long[] longArray, int startIndex, int finishIndex)
+        {
+            long Sum = 0;
+            for (int i = startIndex; i < finishIndex; i++)
+            {
+                Sum += longArray[i];
+            }
+            return Sum;
+        }
+
+        /// <summary>
+        /// Параллельное вычисление суммы элементов
+        /// </summary>
+        /// <param name="longArray"></param>
+        /// <param name="threadNumber">Кол-во параллельных потоков</param>
+        /// <returns></returns>
+        public async Task<long> GetSumAsync(long[] longArray, int threadNumber)
+        {
+            List<Task<long>> paralleltasks = new List<Task<long>>();
+            for (int i = 0; i < threadNumber; i++)
+            {
+                int startIndex = i * longArray.Length / threadNumber;
+                int finishIndex = startIndex + longArray.Length / threadNumber;
+                paralleltasks.Add(GetSumInPartArray(longArray, startIndex, finishIndex));
+            }
+            long[] SpacesCountArr = await Task.WhenAll(paralleltasks);
+            return SpacesCountArr.Sum();
+        }
+        
+        /// <summary>
+        /// Вычисление суммы элементов с помощью PLINQ
+        /// </summary>
+        /// <param name="longArray"></param>
+        /// <param name="threadNumber">Макс кол-во паралелльных потоков</param>
+        /// <returns></returns>
+        public long GetSumAsParallel(long[] longArray, int threadNumber)
+        {
+            long result = longArray.AsParallel().WithMergeOptions(ParallelMergeOptions.AutoBuffered).WithDegreeOfParallelism(threadNumber).Sum();
+            return result;
+        }
+    }
+}
---
+++ HW_Parallel/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+namespace HW_Parallel
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Делегат на расчет суммы элементов массива
+        /// </summary>
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            long[] array = GenerateArray(100000000);
+
+
+            Calculator calculator = new Calculator();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            long SumOfArray = calculator.GetSumAsSequential(array);
+            stopwatch.Stop();
+            Console.WriteLine($"Sum - {SumOfArray} Time - {stopwatch.ElapsedMilliseconds}");
+
+
+            stopwatch.Restart();
+            var AsyncResult = calculator.GetSumAsync(array, 10);
+            stopwatch.Stop();
+            long result = AsyncResult.Result;
+            Console.WriteLine($"Sum - {result} Time - {stopwatch.ElapsedMilliseconds}");
+
+            stopwatch.Restart();
+            var ParallelResult = calculator.GetSumAsParallel(array, 5);
+            stopwatch.Stop();
+            Console.WriteLine($"Sum - {ParallelResult} Time - {stopwatch.ElapsedMilliseconds}");
+        }
+
+        private static long[] GenerateArray(int ArrayLength)
+        {
+            long[] array = new long[ArrayLength];
+            Random random = new Random();
+            for (int i = 0; i < ArrayLength; i++)
+            {
+                array[i] = random.Next();
+            }
+            return array;
+        }
+    }
+}
